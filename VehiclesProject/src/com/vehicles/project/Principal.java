f5302561f47a1f2120dcf1bfd0ff7a657b673111
cotoflux <guaupelu@hotmail.com>
2020-04-29 12:26:12 +0200

    Acabo Fase2

--- VehiclesProject/src/com/vehicles/project/Principal.java
+++ VehiclesProject/src/com/vehicles/project/Principal.java
@@ -19,12 +19,9 @@ public class Principal {
 	    Matcher mat = pat.matcher(matriculaUsuari);
 		
 		if(mat.matches()) {
+			
 			matriculaConfirmada = matriculaUsuari;
-			
-			
-			//Introduim codig
-			
-			
+						
 			Scanner pregunta2 = new Scanner(System.in);
 			String laMarca;
 			System.out.println("Siusplau posa la marca del teu cotxe: ");
@@ -52,48 +49,63 @@ public class Principal {
 				System.out.println("Siusplau dona'm el diàmetre de les rodes delanteres: ");
 				double diametreRodesDevant = pregunta5.nextDouble();
 				
-				Wheel rueda1 = new Wheel(rodesMarcaDevant, diametreRodesDevant);
-				Wheel rueda2 = new Wheel(rodesMarcaDevant, diametreRodesDevant);
-				wheelsDelanteras.add(rueda1);
-				wheelsDelanteras.add(rueda2);
-				
-				
-				System.out.println(wheelsDelanteras.size());
-
-				
-			    for (Wheel i : wheelsDelanteras) {
-			        System.out.println("Rodes delanteres: " + i);
-			      }
+				if(diametreRodesDevant>0.4 && diametreRodesDevant<4.0){
+					System.out.println("Diamtre correcter");
+					
+					//Inicio diametro
+					
+							Wheel rueda1 = new Wheel(rodesMarcaDevant, diametreRodesDevant);
+							Wheel rueda2 = new Wheel(rodesMarcaDevant, diametreRodesDevant);
+							wheelsDelanteras.add(rueda1);
+							wheelsDelanteras.add(rueda2);
+							
+							
+							System.out.println(wheelsDelanteras.size());
+		
+							
+						    for (Wheel i : wheelsDelanteras) {
+						        System.out.println("Rodes delanteres: " + i);
+						      }
+									
+						List<Wheel> wheelsTraseras = new ArrayList<Wheel>();
 						
-			List<Wheel> wheelsTraseras = new ArrayList<Wheel>();
-			
-				Scanner pregunta6 = new Scanner(System.in);
-				System.out.println("Siusplau dona'm la marca de les rodes traseres: ");
-				String rodesMarcaTraseres = pregunta6.nextLine();
-			
-				Scanner pregunta7 = new Scanner(System.in);
-				System.out.println("Siusplau dona'm el diàmetre de les rodes traseres: ");
-				double diametreRodesTraseres = pregunta7.nextDouble();
-				
-				Wheel añadeRuedasTraseres1 = new Wheel(rodesMarcaTraseres, diametreRodesTraseres);
-				Wheel añadeRuedasTraseres2 = new Wheel(rodesMarcaTraseres, diametreRodesTraseres);
-				
-				wheelsTraseras.add(añadeRuedasTraseres1);
-				wheelsTraseras.add(añadeRuedasTraseres2);
-				
-				System.out.println(wheelsTraseras.size());
-
-				
-			    for (Wheel j : wheelsTraseras) {
-			        System.out.println("Rodes Traseres" + j);
-			    }
-			
-				try {
-					cotxeUsuari.addWheels(wheelsDelanteras, wheelsTraseras);
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+							Scanner pregunta6 = new Scanner(System.in);
+							System.out.println("Siusplau dona'm la marca de les rodes traseres: ");
+							String rodesMarcaTraseres = pregunta6.nextLine();
+						
+							Scanner pregunta7 = new Scanner(System.in);
+							System.out.println("Siusplau dona'm el diàmetre de les rodes traseres: ");
+							double diametreRodesTraseres = pregunta7.nextDouble();
+							
+							Wheel añadeRuedasTraseres1 = new Wheel(rodesMarcaTraseres, diametreRodesTraseres);
+							Wheel añadeRuedasTraseres2 = new Wheel(rodesMarcaTraseres, diametreRodesTraseres);
+							
+							wheelsTraseras.add(añadeRuedasTraseres1);
+							wheelsTraseras.add(añadeRuedasTraseres2);
+							
+							System.out.println(wheelsTraseras.size());
+		
+							
+						    for (Wheel j : wheelsTraseras) {
+						        System.out.println("Rodes Traseres" + j);
+						    }
+						
+							try {
+								cotxeUsuari.addWheels(wheelsDelanteras, wheelsTraseras);
+							} catch (Exception e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+					
+					
+					//Fin diametro
+					
+					 
+				} else {
+					System.out.println("El diametro no es correcto, vuelve a reiniciar el programa.");
 				}
+				
+				
 			
 			
 			//Fi introduccio codig
